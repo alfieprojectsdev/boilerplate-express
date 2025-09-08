ce1256f1a25838811a752e9238c4d0cb76c141d9
alfieprojectsdev <alfieprojects.dev@gmail.com>
2025-09-08 14:55:34 +0800

    myAPP modified to serve Hello Express for FCC

--- myApp.js
+++ myApp.js
@@ -3,10 +3,10 @@
 let express = require('express');
 let app = express();
 
-console.log("Hello World");
+console.log("Hello Express");
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.send("Hello Express");
 });
 
 module.exports = app;
